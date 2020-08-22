fbe6cab843d534c6d6598a53c118efde4a45aeb4
丸谷秀一(仕事用) <mil575045.shuichi-marutani@maruyashuuichinoMacBook-Air.local>
2020-08-22 22:24:27 +0900

    【追加】改行、スペースを消す

--- src/App.java
+++ src/App.java
@@ -7,7 +7,16 @@ public class App {
         FileReader fis = new FileReader("data.txt");
         FileWriter fos = new FileWriter("newData.txt");
         while ((data = fis.read()) != -1) {
+            // char charData = (char)data;
+
+            // 10 = 改行, 32 = スペース
+            if(data == 10 || data == 32){
+                continue;
+            }
+            
             fos.write(data);
+            // System.out.println(data);
+            // System.out.println(charData);
         }
         fos.flush();
         fis.close();
