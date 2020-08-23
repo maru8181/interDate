c3897ee330f84e678a05fbf023c32aa4317e9d6f
丸谷秀一(仕事用) <mil575045.shuichi-marutani@maruyashuuichinoMacBook-Air.local>
2020-08-23 12:01:03 +0900

    【追加、修正】バッファリングフィルタ導入

--- src/App.java
+++ src/App.java
@@ -2,56 +2,65 @@ import java.io.*;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        int data;
+        // int data;
 
-        FileReader fis = new FileReader("data.txt");
-        FileWriter fos = new FileWriter("newData.txt");
-        while ((data = fis.read()) != -1) {
-            char charData = (char)data;
+        FileReader fr = new FileReader("data.txt");
+        BufferedReader br = new BufferedReader(fr);
+        FileWriter fw = new FileWriter("newData.txt");
 
-            // 10 = 改行, 32 = スペース, 12288 = 全角スペース
-            // 改行、スペースをとばす
-            if(data == 10 || data == 32 || data == 12288){
-                continue;
-            }
+        String line = null;
+
+        while((line = br.readLine()) != null) {
+            System.out.println(line);
+        // }
+        
+        // while ((data = br.read()) != -1) {
+        //     char charData = (char)data;
+
+        //     // 10 = 改行, 32 = スペース, 12288 = 全角スペース
+        //     // 改行、スペースをとばす
+        //     if(data == 10 || data == 32 || data == 12288){
+        //         continue;
+        //     }
             
-            // 44 = "," , 12289 = "、" 
-            // "," を "、" に変換
-            if(data == 44) {
-                data = 12289;
-            }
+        //     // 44 = "," , 12289 = "、" 
+        //     // "," を "、" に変換
+        //     if(data == 44) {
+        //         data = 12289;
+        //     }
 
-            // 12304 = "【"
-            // "【" の前に改行を入れる
-            if(data == 12304) {
-                data = 10;
-                fos.write(data);
-                data = 12304;
-            }
+        //     // 12304 = "【"
+        //     // "【" の前に改行を入れる
+        //     if(data == 12304) {
+        //         data = 10;
+        //         fw.write(data);
+        //         data = 12304;
+        //     }
 
-            // 12305 = "】"
-            // "】" の後に改行を入れる
-            // 全角スペースで、問題文の先頭を段落づけ
-            if(data == 12305) {
-                fos.write(data);
-                data = 10;
-                fos.write(data);
-                fos.write(data);
-                data = 12288;
-            }
+        //     // 12305 = "】"
+        //     // "】" の後に改行を入れる
+        //     // 全角スペースで、問題文の先頭を段落づけ
+        //     if(data == 12305) {
+        //         fw.write(data);
+        //         data = 10;
+        //         fw.write(data);
+        //         fw.write(data);
+        //         data = 12288;
+        //     }
 
-            // 48 = "0" , 57 = "9"
-            if(48 <= data && data <= 57) {
-                data += 65248;
-            }
+        //     // 48 = "0" , 57 = "9" , 65296 = "０"
+        //     // +65248 することで、半角数字を全角数字に変換
+        //     if(48 <= data && data <= 57) {
+        //         data += 65248;
+        //     }
             
-            fos.write(data);
-            System.out.println(data);
-            System.out.println(charData);
+            fw.write(line);
+            // System.out.println(line);
+            // System.out.println(charData);
         }
-        fos.flush();
-        fis.close();
-        fos.close();
+        fw.flush();
+        br.close();
+        fw.close();
 
     }
 }
