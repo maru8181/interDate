f55e8c8dacf355bccd34cfff23fd6a207d3f3ce7
丸谷秀一(仕事用) <mil575045.shuichi-marutani@maruyashuuichinoMacBook-Air.local>
2020-08-23 18:14:33 +0900

    【追加】1桁の数字は全角、2桁以上の数字は半角にするメソッド追加

--- src/App.java
+++ src/App.java
@@ -17,10 +17,11 @@ public class App {
             String line5 = line4.replace(",", "、");
             String line6 = line5.replace("【", "\n【");
             String line7 = line6.replace("】", "】\n\n　");
-            String line8 = Zenkaku(line7);
+            String line8 = Hankaku(line7);
+            String line9 = Zenkaku(line8);
 
-            sb.append(line8);
-            System.out.print(line8);
+            sb.append(line9);
+            System.out.print(line9);
         }
 
         //金額 [0-9],[0-9]{2,} 後で調整
@@ -37,15 +38,57 @@ public class App {
         fw.close();
     }
 
-    public static String Zenkaku(String hankaku) {
+    public static String Hankaku(String zenkaku) {
         StringBuilder sb2 = new StringBuilder();
-        for(int i = 0; i < hankaku.length(); i++){
-            char c = hankaku.charAt(i);
-            if('0' <= c && c <= '9') {
-                c =(char)(c -'0'+'０');
+
+        for(int i = 0; i < zenkaku.length(); i++){
+            char c = zenkaku.charAt(i);
+            if('０' <= c && c <= '９') {
+                c =(char)(c -'０'+'0');
             }
             sb2.append(c);
         }
         return sb2.toString();
     }
+
+    // 1桁の数字は全角、2桁以上の数字は半角にするメソッド
+    public static String Zenkaku(String hankaku) {
+        StringBuilder sb3 = new StringBuilder();
+
+        for(int i = 0; i < hankaku.length(); i++){
+            char c = hankaku.charAt(i);
+
+            if((i == 0) && (hankaku.length() >= 2)) {
+                char c_af = hankaku.charAt(i+1);
+                
+                if(('0' <= c && c <= '9') && !('0' <= c_af && c_af <= '9')) {
+                    c =(char)(c -'0'+'０');
+                }
+
+            }else if((i == hankaku.length()-1) && (hankaku.length() >= 2)) {
+                char c_fr = hankaku.charAt(i-1);
+
+                if(('0' <= c && c <= '9') && !('0' <= c_fr && c_fr <= '9')) {
+                    c =(char)(c -'0'+'０');
+                }
+
+            }else if(((i > 0) && (i < hankaku.length()-1)) && (hankaku.length() >= 2)) {
+                char c_af = hankaku.charAt(i+1);
+                char c_fr = hankaku.charAt(i-1);
+
+                if(('0' <= c && c <= '9') && !(('0' <= c_fr && c_fr <= '9') || ('0' <= c_af && c_af <= '9'))) {
+                    c =(char)(c -'0'+'０');
+                }
+
+            }else if(hankaku.length() == 1) {
+
+                if('0' <= c && c <= '9') {
+                    c =(char)(c -'0'+'０');
+                }
+            }
+            sb3.append(c);
+        }
+        return sb3.toString();
+    }
+    
 }
