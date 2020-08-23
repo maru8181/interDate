f66887102d1debbbc4620d0abcf389e57e9086cf
丸谷秀一(仕事用) <mil575045.shuichi-marutani@maruyashuuichinoMacBook-Air.local>
2020-08-23 15:11:43 +0900

    【削除】コメント削除

--- src/App.java
+++ src/App.java
@@ -2,7 +2,6 @@ import java.io.*;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        // int data;
 
         FileReader fr = new FileReader("data.txt");
         BufferedReader br = new BufferedReader(fr);
@@ -12,57 +11,20 @@ public class App {
         String line = null;
 
         while((line = br.readLine()) != null) {
-            System.out.println(line);
             String line2 = line.replace(" ", "");
             String line3 = line2.replace("　", "");
-            String line4 = line3.replace(",", "、");
-            String line5 = line4.replace("【", "\n【");
-            String line6 = line5.replace("】", "】\n\n　");
-
-        // }
-        
-        // while ((data = br.read()) != -1) {
-        //     char charData = (char)data;
-
-        //     // 10 = 改行, 32 = スペース, 12288 = 全角スペース
-        //     // 改行、スペースをとばす
-        //     if(data == 10 || data == 32 || data == 12288){
-        //         continue;
-        //     }
-            
-        //     // 44 = "," , 12289 = "、" 
-        //     // "," を "、" に変換
-        //     if(data == 44) {
-        //         data = 12289;
-        //     }
-
-        //     // 12304 = "【"
-        //     // "【" の前に改行を入れる
-        //     if(data == 12304) {
-        //         data = 10;
-        //         fw.write(data);
-        //         data = 12304;
-        //     }
-
-        //     // 12305 = "】"
-        //     // "】" の後に改行を入れる
-        //     // 全角スペースで、問題文の先頭を段落づけ
-        //     if(data == 12305) {
-        //         fw.write(data);
-        //         data = 10;
-        //         fw.write(data);
-        //         fw.write(data);
-        //         data = 12288;
-        //     }
+            String line4 = line3.replace("\n", "");
+            String line5 = line4.replace(",", "、");
+            String line6 = line5.replace("【", "\n【");
+            String line7 = line6.replace("】", "】\n\n　");
 
         //     // 48 = "0" , 57 = "9" , 65296 = "０"
         //     // +65248 することで、半角数字を全角数字に変換
         //     if(48 <= data && data <= 57) {
         //         data += 65248;
-        //     }
-            sb.append(line6);
-            // System.out.println(line);
-            // System.out.println(charData);
+
+            sb.append(line7);
+            System.out.println(line6);
         }
         String str = sb.toString();
         fw.write(str);
@@ -70,6 +32,5 @@ public class App {
         fw.flush();
         br.close();
         fw.close();
-
     }
 }
