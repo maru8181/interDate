ef1dec1725c4b5660e0c7e1cf7db1ace774c3fe8
丸谷秀一(仕事用) <mil575045.shuichi-marutani@maruyashuuichinoMacBook-Air.local>
2020-08-23 10:39:54 +0900

    【追加】"【　】"の前後にスペース,改行を入れる

--- src/App.java
+++ src/App.java
@@ -7,16 +7,35 @@ public class App {
         FileReader fis = new FileReader("data.txt");
         FileWriter fos = new FileWriter("newData.txt");
         while ((data = fis.read()) != -1) {
-            // char charData = (char)data;
+            char charData = (char)data;
 
             // 10 = 改行, 32 = スペース
             if(data == 10 || data == 32){
                 continue;
             }
             
+            // 44 = "," , 12289 = "、" 
+            if(data == 44) {
+                data = 12289;
+            }
+
+            // 12304 = "【"
+            if(data == 12304) {
+                data = 10;
+                fos.write(data);
+                data = 12304;
+            }
+
+            // 12305 = "】"
+            if(data == 12305) {
+                fos.write(data);
+                data = 10;
+                fos.write(data);
+            }
+            
             fos.write(data);
-            // System.out.println(data);
-            // System.out.println(charData);
+            System.out.println(data);
+            System.out.println(charData);
         }
         fos.flush();
         fis.close();
