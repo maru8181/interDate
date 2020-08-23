3fdd2ddc9b00152c6571f519d437a3b572e34939
丸谷秀一(仕事用) <mil575045.shuichi-marutani@maruyashuuichinoMacBook-Air.local>
2020-08-23 10:57:32 +0900

    【追加】全角スペースとばす

--- src/App.java
+++ src/App.java
@@ -9,17 +9,20 @@ public class App {
         while ((data = fis.read()) != -1) {
             char charData = (char)data;
 
-            // 10 = 改行, 32 = スペース
-            if(data == 10 || data == 32){
+            // 10 = 改行, 32 = スペース, 12288 = 全角スペース
+            // 改行、スペースをとばす
+            if(data == 10 || data == 32 || data == 12288){
                 continue;
             }
             
             // 44 = "," , 12289 = "、" 
+            // "," を "、" に変換
             if(data == 44) {
                 data = 12289;
             }
 
             // 12304 = "【"
+            // "【" の前に改行を入れる
             if(data == 12304) {
                 data = 10;
                 fos.write(data);
@@ -27,6 +30,8 @@ public class App {
             }
 
             // 12305 = "】"
+            // "】" の後に改行を入れる
+            // 全角スペースで、問題文の先頭を段落づけ
             if(data == 12305) {
                 fos.write(data);
                 data = 10;
