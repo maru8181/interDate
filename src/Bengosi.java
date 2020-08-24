a963225b5334a07f54911e08c4e5ddae8826847a
丸谷秀一(仕事用) <mil575045.shuichi-marutani@maruyashuuichinoMacBook-Air.local>
2020-08-24 21:16:10 +0900

    【追加】ファイル名自動追加機能

--- src/Bengosi.java
+++ src/Bengosi.java
@@ -3,53 +3,65 @@ import java.io.*;
 public class Bengosi {
     public static void main(String[] args) throws Exception {
 
-        FileReader fr = new FileReader("bengosi.txt");
-        // FileReader fr = new FileReader("../beforeFile/bengosi.txt");
-        BufferedReader br = new BufferedReader(fr);
-        FileWriter fw = new FileWriter("newBengosi.txt");
-        // FileWriter fw = new FileWriter("../changedFile/newBengosi.txt");
-        StringBuilder sb = new StringBuilder();
+        File dir = new File("../beforeFile");
+        File[] list = dir.listFiles();
 
-        String line = null;
-
-        while((line = br.readLine()) != null) {
-            String line2 = line.replace(" ", "");
-            String line3 = line2.replace("　", "");
-            String line4 = line3.replace("\n", "");
-            String line5 = line4.replace(",", "，");
-            String line7 = line5.replace("〔", "\n\n\n〔");
-            // String line7 = line6.replace("〕", "〕\n\n　");
-            String line8 = line7.replaceAll("-[０-９]-", "");
-            String line9 = line8.replaceAll("-[0-9]{2,}-", "");
-            String line10 = Hankaku(line9);
-            String line11 = sentakusi(line10);
-            String line12 = Kingaku(line11);
-            String line13 = Space(line12);
-
-            sb.append(line13);
-            // System.out.print(line13);
+        for(int i=0; i<list.length; i++) {
+            System.out.println(list[i].toString());
+            System.out.println(list[i].getName()); 
         }
 
-        String str = sb.toString();
-        // String str2 = str.replace("）１．", "）\n\n　１．");
-        String str3 = str.replace("１．", "\n\n　１．");
-        String str4 = str3.replace("２．", "\n　２．");
-        String str5 = str4.replace("３．", "\n　３．");
-        String str6 = str5.replace("４．", "\n　４．");
-        String str7 = str6.replace("５．", "\n　５．");
-        String str8 = str7.replace("ア．", "\n\n　ア．");
-        String str9 = str8.replace("イ．", "\n　イ．");
-        String str10 = str9.replace("ウ．", "\n　ウ．");
-        String str11 = str10.replace("エ．", "\n　エ．");
-        String str12 = str11.replace("オ．", "\n　オ．");
-        String str13 = str12.replace("：２）", "：２）\n\n　");
-        String str14 = str13.replace("：３）", "：３）\n\n　");
+        for(int i = 0; i < list.length; i++) {
 
-        fw.write(str14);
-        fw.flush();
+            // FileReader fr = new FileReader("bengosi.txt");
+            FileReader fr = new FileReader(list[i].toString());
+            BufferedReader br = new BufferedReader(fr);
+            // FileWriter fw = new FileWriter("newBengosi.txt");
+            FileWriter fw = new FileWriter("../changedFile/[new]" + list[i].getName());
+            StringBuilder sb = new StringBuilder();
 
-        br.close();
-        fw.close();
+            String line = null;
+
+            while((line = br.readLine()) != null) {
+                String line2 = line.replace(" ", "");
+                String line3 = line2.replace("　", "");
+                String line4 = line3.replace("\n", "");
+                String line5 = line4.replace(",", "，");
+                String line7 = line5.replace("〔", "\n\n\n〔");
+                // String line7 = line6.replace("〕", "〕\n\n　");
+                String line8 = line7.replaceAll("-[０-９]-", "");
+                String line9 = line8.replaceAll("-[0-9]{2,}-", "");
+                String line10 = Hankaku(line9);
+                String line11 = sentakusi(line10);
+                String line12 = Kingaku(line11);
+                String line13 = Space(line12);
+
+                sb.append(line13);
+                // System.out.print(line13);
+            }
+
+            String str = sb.toString();
+            // String str2 = str.replace("）１．", "）\n\n　１．");
+            String str3 = str.replace("１．", "\n\n　１．");
+            String str4 = str3.replace("２．", "\n　２．");
+            String str5 = str4.replace("３．", "\n　３．");
+            String str6 = str5.replace("４．", "\n　４．");
+            String str7 = str6.replace("５．", "\n　５．");
+            String str8 = str7.replace("ア．", "\n\n　ア．");
+            String str9 = str8.replace("イ．", "\n　イ．");
+            String str10 = str9.replace("ウ．", "\n　ウ．");
+            String str11 = str10.replace("エ．", "\n　エ．");
+            String str12 = str11.replace("オ．", "\n　オ．");
+            String str13 = str12.replace("：２）", "：２）\n\n　");
+            String str14 = str13.replace("：３）", "：３）\n\n　");
+
+            fw.write(str14);
+            fw.flush();
+
+            br.close();
+            fw.close();
+            
+        }
     }
 
     // すべての数字を半角にするメソッド
