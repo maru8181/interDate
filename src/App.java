aaba0ac047bf9c5dc0a81aa5d59e06f02b8a1813
丸谷秀一(仕事用) <mil575045.shuichi-marutani@maruyashuuichinoMacBook-Air.local>
2020-08-24 15:28:41 +0900

    【追加】金額を半角数字にするメソッド追加

--- src/App.java
+++ src/App.java
@@ -19,12 +19,12 @@ public class App {
             String line7 = line6.replace("】", "】\n\n　");
             String line8 = Hankaku(line7);
             String line9 = Zenkaku(line8);
+            String line10 = Kingaku(line9);
 
-            sb.append(line9);
-            System.out.print(line9);
+            sb.append(line10);
+            System.out.print(line10);
         }
 
-        //金額 [0-9],[0-9]{2,} 後で調整
         String str = sb.toString();
         String str2 = str.replace("。１、", "。\n\n　１、");
         String str3 = str2.replace("２、", "\n　２、");
@@ -92,4 +92,37 @@ public class App {
         return sb3.toString();
     }
     
+    // 金額を半角数字にするメソッド    [0-9]{1,3},[0-9]{3} を金額と判定
+    public static String Kingaku(String zenkaku) {
+        StringBuilder sb4 = new StringBuilder();
+
+        for(int i = 0; i < zenkaku.length(); i++){
+            char c = zenkaku.charAt(i);
+
+            if((i <= zenkaku.length()-5) && (zenkaku.length() >= 5)) {
+                char c_af = zenkaku.charAt(i+1);
+                char c_af2 = zenkaku.charAt(i+2);
+                char c_af3 = zenkaku.charAt(i+3);
+                char c_af4 = zenkaku.charAt(i+4);
+
+                if(('０' <= c && c <= '９') && (c_af == '、') && ('0' <= c_af2 && c_af2 <= '9') && ('0' <= c_af3 && c_af3 <= '9') && ('0' <= c_af4 && c_af4 <= '9')) {
+                    c =(char)(c -'０'+'0');
+                }
+            }
+
+            if((i >= 1) && (i <= zenkaku.length()-4) && (zenkaku.length() >= 5)) {
+                char c_bf = zenkaku.charAt(i-1);
+                char c_af = zenkaku.charAt(i+1);
+                char c_af2 = zenkaku.charAt(i+2);
+                char c_af3 = zenkaku.charAt(i+3);
+
+                if((('０' <= c_bf && c_bf <= '９') || ('0' <= c_bf && c_bf <= '9')) && (c == '、') && ('0' <= c_af && c_af <= '9') && ('0' <= c_af2 && c_af2 <= '9') && ('0' <= c_af3 && c_af3 <= '9')) {
+                    c = ',';
+                }
+            }
+
+            sb4.append(c);
+        }
+        return sb4.toString();
+    }
 }
