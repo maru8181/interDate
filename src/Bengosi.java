4684d6da9881e5be79af0654c6ebb70f6b2fae18
丸谷秀一(仕事用) <mil575045.shuichi-marutani@maruyashuuichinoMacBook-Air.local>
2020-08-25 21:36:45 +0900

    【追加】会話の箇所、処理追加

--- src/Bengosi.java
+++ src/Bengosi.java
@@ -20,23 +20,29 @@ public class Bengosi {
                 String line4 = line3.replace("\n", "");
                 String line5 = line4.replace(",", "，");
                 String line6 = line5.replace("〔", "\n\n\n〔");
-                String line10 = line6.replace("（参照", "\n\n　（参照");
+                String line20 = line6.replace("（参照", "\n\n　（参照");
 
-                String line11 = Util.hankaku(line10);
-                String line12 = Util.henkan(line11);
-                String line13 = Util.kingaku(line12);
-                String line14 = Util.space(line13);
-                String line15 = Util.kaigyou(line14);
-                String line16 = Util.haiten(line15);
-                String line20 = Util.marubatu(line16);
+                String line21 = Util.hankaku(line20);
+                String line22 = Util.henkan(line21);
+                String line23 = Util.kingaku(line22);
+                String line24 = Util.space(line23);
+                String line25 = Util.kaigyou(line24);
+                String line26 = Util.haiten(line25);
+                String line50 = Util.marubatu(line26);
 
-                String line21 = line20.replaceAll("-[０-９]-", "");
-                String lineFinal = line21.replaceAll("-[0-9]{2,}-", "");
+                String line51 = line50.replaceAll("-[０-９]-", "");
+                String line100 = line51.replaceAll("-[0-9]{2,}-", "");
 
-                sb.append(lineFinal);
+                sb.append(line100);
             }
 
-            fw.write(sb.toString());
+            String str = sb.toString();
+            String str2 = str.replace("【会話】教授：", "\n\n　【会話】\n　教　授：");
+            String str3 = str2.replace("。学生", "。\n　学生");
+            String str4 = str3.replace("。教授：", "。\n　教　授：");
+            String strFinal = str4.replace("【語句群】\n\n　ア", "\n\n　【語句群】\n\n　ア");
+
+            fw.write(strFinal);
             fw.flush();
             br.close();
             fw.close();
